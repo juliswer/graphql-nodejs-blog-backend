c7b3b4e8dd8161bbf53ebd6a7b5fa866f57ec25d
Julian Swerdlin <julianswer36@gmail.com>
2022-01-14 20:45:51 +0100

    uploading base files

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const {graphqlHTTP} = require('express-graphql');
+const schema = require('./graphql/schema')
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('welcome to my graphql api')
+})
+
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: true
+}))
+
+app.listen(3000);
+console.log('Server is running on port 3000');
