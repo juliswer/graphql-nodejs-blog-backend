94d517068382bae60bc2e831f3546f96472f3f8d
Julian Swerdlin <julianswer36@gmail.com>
2022-01-14 21:25:58 +0100

    saving a new user in mongodb

--- graphql/mutations.js
+++ graphql/mutations.js
@@ -1,9 +1,24 @@
 const {GraphQLString} = require('graphql')
+const {User} = require('../models')
 
 const register = {
     type: GraphQLString,
     description: 'Register a new user',
-    async resolve() {
+    args: {
+        username: {type: GraphQLString},
+        email: {type: GraphQLString},
+        password: {type: GraphQLString},
+        displayName: {type: GraphQLString}
+    },
+    async resolve(_, args) {
+        const {username, email, password, displayName} = args
+        const newUser = await User.create({
+            username,
+            email,
+            password,
+            displayName
+        })
+        console.log(newUser)
         return 'new user created'
     }
 }
