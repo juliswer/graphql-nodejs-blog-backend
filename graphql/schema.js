1f5622ba4581d57a0835d61a133524615e46455f
Julian Swerdlin <julianswer36@gmail.com>
2022-01-17 03:33:26 +0100

    new

--- graphql/schema.js
+++ graphql/schema.js
@@ -1,12 +1,12 @@
 const {GraphQLSchema, GraphQLObjectType} = require('graphql')
-const {hello} = require('./queries')
+const {users} = require('./queries')
 const {register, login} = require('./mutations')
 
 const QueryType = new GraphQLObjectType({
     name: 'QueryType',
     description: 'This is the root query',
     fields: {
-        hello
+        users 
     }
 })
 
